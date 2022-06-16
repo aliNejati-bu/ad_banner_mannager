917aac8a8c6b8515ebb37f04424db4462d702655
aliNejati-bu <electrocellco@gmail.com>
2022-06-16 17:19:20 +0430

    add banner mongoose model.

---
+++ src/Data/Entities/Banner.ts
@@ -0,0 +1,11 @@
+export class Banner {
+    constructor(
+        public _id: string,
+        public name: string,
+        public image: string,
+        public url: string,
+        public createdAt: Date,
+        public updatedAt: Date,
+    ) {
+    }
+}
---
+++ src/Data/MongooseDatabaseService/Model/MongooseBannerModel.ts
@@ -0,0 +1,34 @@
+import * as mongoose from 'mongoose';
+import {Banner} from "../../Entities/Banner";
+
+const bannerSchema = new mongoose.Schema<Banner>({
+    _id: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    }
+});
+
+
+export default mongoose.model<Banner>("Banner", bannerSchema);
