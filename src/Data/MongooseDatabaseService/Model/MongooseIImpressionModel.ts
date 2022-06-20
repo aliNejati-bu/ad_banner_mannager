f1f12d5f80abe758e9443eaee080c4243f799ce2
aliNejati-bu <electrocellco@gmail.com>
2022-06-21 00:04:58 +0430

    add MongooseIImpressionModel

---
+++ src/Data/MongooseDatabaseService/Model/MongooseIImpressionModel.ts
@@ -0,0 +1,42 @@
+import * as mongoose from 'mongoose';
+import {Impression} from "../../Entities/Impression";
+
+
+const bannerSchema = new mongoose.Schema<Impression>({
+    _id: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    createdAt: {
+        type: Date,
+        required: true
+    },
+    updatedAt: {
+        type: Date,
+        required: true
+
+    },
+    adPlace: {
+        type: String,
+        required: true,
+
+    },
+    game: {
+        type: String,
+        required: true
+    },
+    banner: {
+        type: String,
+        required: true
+
+    },
+    country: {
+        type: String,
+        required: true
+    }
+});
+
+
+export default mongoose.model<Impression>("Impression", bannerSchema);
